d918fdeba34c762f3a7bf741623d31e11466fa5d
Chris McKenzie <kristopolous@yahoo.com>
2020-05-10 04:36:36 -0700

    cleanup

--- tools/rating-distrib.py
+++ tools/rating-distrib.py
@@ -5,21 +5,20 @@ from functools import reduce
 thresh = int(sys.argv[1]) if len(sys.argv) > 1 else 5
 width = 70
 longestLabel = 0
-
 labelMap = {}
 fracList = []
+rateList = ['__rating_5', '__rating_4', '__rating_3', '__purge']
+
 with open('.listen_done', 'r') as f:
   for line in f.readlines():
     parts = line.split(' ')
     label = parts[0].split('/')[0]
     rating = parts[1].strip()
-    if rating in ['__rating_2', '__purge']: 
-      rating = '__purge'
 
-    elif rating == '__skipping':
+    if rating == '__skipping':
       rating = '__rating_3'
 
-    if rating not in ['__rating_5', '__rating_4', '__rating_3', '__purge']:
+    if rating not in rateList:
       continue
 
     if not label in labelMap:
@@ -28,7 +27,7 @@ with open('.listen_done', 'r') as f:
     labelMap[label][rating] = (labelMap[label].get(rating) or 0) + 1
 
 for label in labelMap:
-  total = reduce(lambda x,y: x + y, labelMap[label].values(), 0)
+  total = reduce(lambda x, y: x + y, labelMap[label].values(), 0)
   if total > thresh:
     longestLabel = max(longestLabel, len(label))
     row = {'weight': 0, 'label': label, 'total': total}
@@ -36,7 +35,7 @@ for label in labelMap:
       row[k] = v / total
     
     num = 4
-    for i in ['__rating_5', '__rating_4', '__rating_3', '__purge']:
+    for i in rateList:
       row['weight'] = num * (row.get(i) or 0)
       num -= 1
 
@@ -48,11 +47,10 @@ for row in fracSort:
   graph = ''
   accum = 0
   start = 9608
-  for i in ['__rating_5', '__rating_4', '__rating_3', '__purge']:
+  for i in rateList:
     accum += row.get(i) or 0
     inst = round(accum * width) - len(graph)
     draw = '.' if i == '__purge' else chr(start)
-
     graph += draw * inst
     start += 3
 
