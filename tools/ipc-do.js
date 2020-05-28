d6b4d0b800a997446cca7aea4d7000edb319e315
Chris McKenzie <kristopolous@yahoo.com>
2020-05-28 00:17:05 -0700

    ipc system working

--- tools/ipc-do.js
+++ tools/ipc-do.js
@@ -1,45 +1,65 @@
 const net = require('net');
 const client = net.createConnection('/tmp/mpvsocket')
-const command = process.argv[process.argv.length - 1];
+var command = process.argv[process.argv.length - 1];
 
 const commandMap = {
   pause: ['set_property', 'pause', true],
-  play: ['set_property', 'pause', false]
+  play: ['set_property', 'pause', false],
+  time: ['get_property', 'time-pos'],
+  playlist: ['get_property', 'playlist-pos'],
+  getpause: ['get_property', 'pause']
 }
 
-const expectMap = {
-  pause: 'pause',
-  play: 'unpause'
-};
+const quitList = [ 'pause', 'playback-restart', 'unpause' ];
+var dataCb = false;
+var direction = ['back','prev'].includes(command) ? -1 : 1;
 
-var expect = false;
+function send(list) {
+  var towrite = JSON.stringify({command: list});
+  client.write(Buffer.from(towrite + "\n", 'utf-8'));
+}
 
-client.on('connect', (s) => {
-  var towrite = {};
-  console.log(command);
+if (command == 'pauseplay') {
+  dataCb = function(state) {
+    send([ 'set_property', 'pause', !state ]);
+  }
+  command = 'getpause';
+} else if (['prev','next'].includes(command)) {
+  dataCb = function(pos) {
+    let newpos = pos + direction;
+    if(newpos < 0) {
+      process.exit();
+    }
 
-  towrite.command = commandMap[command];
-  expect = expectMap[command];
+    send([ 'set_property', 'playlist-pos', newpos ]);
+  }
+  command = 'playlist';
+} else if (['back','forward'].includes(command)) {
 
-  if(towrite.command) {
-    var towrite = JSON.stringify(towrite);
-    client.write(Buffer.from(towrite + "\n", 'utf-8'));
+  dataCb = function(pos) {
+    send([ 'set_property', 'time-pos', pos + (10 * direction) ]);
+  }
+  command = 'time';
+}
+
+client.on('connect', () => {
+  if(commandMap[command]) {
+    send(commandMap[command]);
   }
 });
 
 client.on('data', (data) => {
-  data.toString('utf8').split('\n').forEach(rowRaw => {
-    if (rowRaw.length) {
-      let row = JSON.parse(rowRaw);
-      if(row.event === expect) {
-        process.exit();
-      }
-      console.log(row);
-    }
-  });
-});
+  data.toString('utf8').trim().split('\n').forEach(rowRaw => {
+    let row = JSON.parse(rowRaw);
 
-client.on('close', (data) => {
-  console.log(data);
+    if(quitList.includes(row.event)) {
+      process.exit();
+    } 
+
+    if('data' in row && dataCb) {
+      dataCb(row.data);
+    }
+    console.log(row);
+  });
 });
 
