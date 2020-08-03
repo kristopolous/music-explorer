db43a8d7ed96636d3fee15ca8e0beec8ce8653fa
Chris McKenzie <kristopolous@yahoo.com>
2020-08-02 21:19:22 -0700

    allowing for a filter on the midicontrols

--- tools/midiread.py
+++ tools/midiread.py
@@ -10,9 +10,13 @@ if os.path.exists('midiconfig.ini'):
   config.read('midiconfig.ini')
   
 controlMapping = { }
+optionMap = {}
 valueMap = {}
 lastValueMap = {}
 todoMap = {}
+
+for key in config['config']:
+  optionMap[key] = config['config'][key]
 
 for key in config['mappings']:
   code = int(config['mappings'][key])
@@ -27,7 +31,15 @@ sys.exit()
 usbDevice = os.popen('pactl list sinks | grep -C 4 MPOW | grep -E "^S" | cut -d "#" -f 2').read().strip()
 
 lastval = False
-cmd = "amidi -l | tail -1 | awk ' { print $2 }'"
+ifilter = ''
+
+if optionMap.get('device'):
+  ifilter = "grep '{}'".format(optionMap.get('device'))
+else:
+  ifilter = 'tail -1'
+
+cmd = "amidi -l | %s | awk ' { print $2 }'" % (ifilter)
+print(cmd)
 
 deviceNumber = os.popen(cmd).read().strip()
 if not deviceNumber or deviceNumber == 'Device':
