93016daad06790cd59c7e43635852c9852da2851
Chris McKenzie <kristopolous@yahoo.com>
2020-08-03 15:50:24 -0700

    more prefs work

--- tools/prefs.sh
+++ tools/prefs.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-player=mplayer
-player_opts=
-#player_opts=--script=$DIR/quit-on-error.lua --term-playing-msg='\n${media-title}' --input-ipc-server=/tmp/mpvsocket 
+player=mpv
+player_opts="--script=$DIR/quit-on-error.lua "'--term-playing-msg=\n${media-title} --input-ipc-server=/tmp/mpvsocket'
