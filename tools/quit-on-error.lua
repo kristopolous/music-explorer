2f3f98b622340f05481f1987420d38d545362fcb
Chris McKenzie <kristopolous@yahoo.com>
2020-08-17 17:20:52 -0700

    exiting only on specific stream errors

--- tools/quit-on-error.lua
+++ tools/quit-on-error.lua
@@ -1,8 +1,23 @@
 local os = require 'os'
 mp.enable_messages('error')
+function tprint (tbl, indent)
+  if not indent then indent = 0 end
+  for k, v in pairs(tbl) do
+    formatting = string.rep("  ", indent) .. k .. ": "
+    if type(v) == "table" then
+      print(formatting)
+      tprint(v, indent+1)
+    elseif type(v) == 'boolean' then
+      print(formatting .. tostring(v))      
+    else
+      print(formatting .. v)
+    end
+  end
+end
 
 function lg(e) 
-  if e.level == "error" then
+  tprint(e,2)
+  if e.level == "error" and e.prefix == 'stream' then
     print("Exiting...")
     mp.command('quit 2')
   end
