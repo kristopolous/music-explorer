902dbb9c51f3d9fe9ab4b8596f04734215406a83
Chris McKenzie <kristopolous@yahoo.com>
2020-11-16 01:40:49 -0800

    get_urls refactoring

--- tools/lib.sh
+++ tools/lib.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-get_urls() {
-   youtube-dl --get-duration --get-filename -gf mp3-128 -- "$1" | awk -f $DIR/ytdl2m3u.awk > "$2"/url-list.m3u
-   echo $? > "$2"/exit-code
+
+[[ -z "$DIR" ]] && DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+_get_urls() {
+  youtube-dl --get-duration --get-filename -gf mp3-128 -- "$1" | awk -f $DIR/ytdl2m3u.awk > "$2"
 }
+get_urls() {
+  _get_urls $1 "$2/url-list.m3u"
+  echo $? > "$2"/exit-code
+}
+
 get_mp3s() {
   {
     cd "$2"
