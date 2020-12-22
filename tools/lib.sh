1472c05e8c6c65dcab731d3a4c02c6ee98ee4e61
Chris McKenzie <kristopolous@yahoo.com>
2020-12-21 20:03:23 -0800

    Working on the new playlist system

--- tools/lib.sh
+++ tools/lib.sh
@@ -2,18 +2,30 @@
 
 [[ -z "$DIR" ]] && DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+PLAYLIST=playlist.m3u
+
 _get_urls() {
   youtube-dl --get-duration --get-filename -gf mp3-128 -- "$1" | awk -f $DIR/ytdl2m3u.awk > "$2"
 }
 get_urls() {
-  _get_urls $1 "$2/playlist.m3u"
+  _get_urls $1 "$2/$PLAYLIST"
   echo $? > "$2"/exit-code
 }
 
+get_playlist() {
+  [[ -e $PLAYLIST ]] || youtube-dl -eif mp3-128 -- "$1" |\
+    sed -E 's/^([^-]*)\s?-?\s?(.*$)/compgen -G "\0"* || compgen -G "\2"*;/' | /bin/bash > $PLAYLIST
+
+  if [[ ! -s $PLAYLIST ]]; then 
+    echo "unable to create $PLAYLIST, doing fallback"
+    ls -1 > $PLAYLIST 
+  fi
+}
+
 get_mp3s() {
-  {
+  (
     cd "$2"
     youtube-dl -f mp3-128 -- "$1"
     echo $? > exit-code
-  }
+  )
 }
