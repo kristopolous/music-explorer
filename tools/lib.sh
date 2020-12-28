b444355344951c3ca34c94b341bbfecd3faf96a9
Chris McKenzie <kristopolous@yahoo.com>
2020-12-27 16:43:36 -0800

    cleanup, trying to avoid an mpv bug

--- tools/lib.sh
+++ tools/lib.sh
@@ -13,12 +13,27 @@ get_urls() {
 }
 
 get_playlist() {
+  dbg=/tmp/playlist-interim-$(date +%s)
   [[ -e $PLAYLIST ]] || youtube-dl -eif mp3-128 -- "$1" |\
-    sed -E 's/^([^-]*)\s?-?\s?(.*$)/compgen -G "\0"* || compgen -G "\2"*;/' | /bin/bash | grep mp3 > $PLAYLIST
+    sed -E 's/^([^-]*)\s?-?\s?(.*$)/compgen -G "\0"* || compgen -G "\2"*;/' > $dbg 
+
+  echo $dbg
+  tomatch=$(cat $dbg | wc -l)
+
+  /bin/bash $dbg | grep mp3 > $PLAYLIST
+
+  matched=$(cat $PLAYLIST | wc -l)
+
+  echo $tomatch " --- " $matched
+
+  if [[ $tomatch != $matched ]]; then
+    echo "Woah hold on"
+    exit
+  fi
 
   if [[ ! -s $PLAYLIST ]]; then 
     echo "unable to create $PLAYLIST, doing fallback"
-    ls -1 *.mp3 > $PLAYLIST 
+    ls -1 "*.mp3" > $PLAYLIST 
   fi
 }
 
