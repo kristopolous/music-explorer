26f6ced131d88b42955d0c5a9c1e606ba059bc9c
Chris McKenzie <kristopolous@yahoo.com>
2021-08-09 19:12:25 -0700

    rolling album art into the lib

--- tools/lib.sh
+++ tools/lib.sh
@@ -46,9 +46,25 @@ function headline {
 function info {
   echo -e "\t$1"
 }
+
 function status {
   [[ -n "$2" ]] && echo
   echo -e "\t\t$1"
+}
+
+album_art() {
+  for i in */*; do
+    [[ ! -d $i ]] && continue
+
+    if [[ ! -e $i/no && ! -e $i/album-art.jpg ]] ; then
+
+      album=$(resolve $i)
+
+      url=$(curl -Ls $album | grep -A 4 'tralbumArt' | grep popupImage | grep -Po 'https:.*[jp][pn]g')
+      [[ -n "$url" ]] && curl -so $i/album-art.jpg $url
+      echo "$url => $album"
+    fi
+  done
 }
 
 check_url() {
