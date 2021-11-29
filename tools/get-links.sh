b904a862cf859aa4d07421ecf725bffbdda2193d
Chris McKenzie <kristopolous@yahoo.com>
2021-11-28 20:37:18 -0800

    folding get-links to the lib

--- tools/get-links.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat .listen_done | grep rating_5 | awk ' { print $1 } ' | while read line; do
-  if [[ -e $line/domain ]]; then
-    cat $line/domain
-  else
-    echo $line | awk -F \/ ' { print "https://"$1".bandcamp.com/album/"$2 }' 
-  fi
-done
--- tools/lib.sh
+++ tools/lib.sh
@@ -408,3 +408,13 @@ details() {
     echo "----- ($pid) ------"
   done
 }
+
+get_links() {
+  cat .listen_done | grep rating_5 | awk ' { print $1 } ' | while read line; do
+    if [[ -e $line/domain ]]; then
+      cat $line/domain
+    else
+      echo $line | awk -F \/ ' { print "https://"$1".bandcamp.com/album/"$2 }' 
+    fi
+  done
+}
