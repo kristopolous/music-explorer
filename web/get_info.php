f124fc776abebd15dcb21706af966bb2c3e809a5
Chris McKenzie <kristopolous@yahoo.com>
2022-03-10 13:51:42 -0800

    adding a search

--- web/get_info.php
+++ web/get_info.php
@@ -1,5 +1,6 @@
 <?php
 $where = $_GET['p'];
+$search = isset($_GET['q']) ? $_GET['q'] : false;
 $dir = dirname($where);
 
 $domain = file_get_contents("$dir/domain");
@@ -8,6 +9,9 @@ if(!file_exists("$dir/playlist.m3u")) {
   echo "http://9ol.es/$where";
 } else {
   $playlist = file("$dir/playlist.m3u", FILE_IGNORE_NEW_LINES);
+  if($search) {
+    $playlist = preg_grep($search, $playlist);
+  }
   $file = basename($where);
 
   for($ix = 0; $ix < count($playlist); $ix++) {
