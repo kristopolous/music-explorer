0edca2aa77dd2d777b3351171287ac2581ff6411
Chris McKenzie <kristopolous@yahoo.com>
2022-05-31 16:09:02 -0700

    adding a random feature

--- tools/artist-count.zsh
+++ tools/artist-count.zsh
@@ -1,9 +1,21 @@
 #!/usr/bin/zsh
-(
-  for release in $(tail -50000 .listen_done| grep rating_5 | awk ' { print $1 } '); do
-    for track in $release/*mp3([1]); do
-      echo $track
-      id3v2 -R "$track" 
+method1() {
+  (
+    for release in $(tail -50000 .listen_done| grep rating_5 | awk ' { print $1 } '); do
+      for track in $release/*mp3([1]); do
+        echo $track
+        id3v2 -R "$track" 
+      done
     done
-  done
-) | grep TPE2 | sort | sed -E s'/TPE[12]:\s*//g' | uniq -c | sort -n | uniq
+  ) | grep TPE2 | sort | sed -E s'/TPE[12]:\s*//g' | uniq -c | sort -n | uniq
+}
+
+method2() {
+  {
+    for release in $(tail -50000 .listen_done| grep rating_5 | awk ' { print $1 } '); do
+      echo $(basename $release/*mp3([1])) | cut -d '-' -f 1
+    done
+  } | sort | uniq -c | sort -n 
+}
+
+method2
--- web/get_playlist.php
+++ web/get_playlist.php
@@ -2,22 +2,35 @@
 $off = 0;
 $search = isset($_GET['q']) ? $_GET['q'] : false;
 $parts = file('playlist.txt', FILE_IGNORE_NEW_LINES);
-if($search) {
+$ttl = 0;
+$mt = function($m) { return $m; };
+
+if($search == '%rand') {
+  $mt_off = 100 * floor(time() / (60 * 60 * 24));
+  $mt = function($m) use ($ttl, $mt_off){ 
+    global $ttl;
+    mt_srand($m + $mt_off);
+    return mt_rand(0, $ttl);
+  };
+
+} else if($search) {
   $parts = array_values(preg_grep("/$search/i", $parts));
 }
 if(isset($_GET['off'])) {
   $off = $_GET['off'] % count($parts);
 }
+$ttl = count($parts);
 $start = 0;
 $ret = [
-  'ttl' => count($parts),
+  'ttl' => $ttl,
   'q' => $search,
   'res' => [],
   'off' => $off
 ];
 for($ix = $off; $start < 10; $start++) {
-  $ret['res'][] = $parts[$ix];
-  $ix = ($ix + 1) % count($parts);
+  $ix_off = $mt($ix);
+  $ret['res'][] = $parts[$ix_off];
+  $ix = ($ix + 1) % $ttl;
 }
 
 
