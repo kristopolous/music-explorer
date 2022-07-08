25a08bdd0628f1fb4eb5b5d28d46bc3daafede63
Chris McKenzie <kristopolous@yahoo.com>
2022-07-07 17:50:44 -0700

    cleanup

--- web/get_playlist.php
+++ web/get_playlist.php
@@ -1,77 +1,119 @@
 <?php
-$off = 0;
-$search = isset($_GET['q']) ? $_GET['q'] : false;
-$parts = file('playlist.txt', FILE_IGNORE_NEW_LINES);
-$ttl = 0;
-$skip = $_GET['skip'] ?? false;
-$mt = function($m) { return $m; };
+$sql = new PDO('sqlite:playlist.db', false, false, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+$start = microtime(true);
 
-if($search === '.rand') {
-  $mt_off = 100000 * floor(time() / (60 * 60 * 24));
-  $mt = function($m) use ($ttl, $mt_off){ 
-    global $ttl;
-    mt_srand($m + $mt_off);
-    return mt_rand(0, $ttl);
-  };
+function get($qstr, $params = [], $type = false) {
+  global $sql;
 
-} else if($search) {
-  $parts = array_values(preg_grep("/$search/i", $parts));
-}
-$ttl = count($parts);
-
-if($ttl === 0) {
-  $ret = [
-    'ttl' => $ttl,
-    'q' => $search,
-    'res' => [],
-    'off' => $off
-  ];
-} else {
-  if(isset($_GET['off'])) {
-    $off = $_GET['off'] % $ttl;
+  $where_list = array_map(fn($v) => "$v = :$v", array_keys($params));
+  if(isset($_GET['q'])) {
+    $where_list[] = "path like :q";
+    $params['q'] = "%${_GET['q']}%";
   }
-  if($skip) {
-    if(!$off) {
-      $off = 0;
+  if(!empty($where_list)) { 
+    $qstr .= " where " . implode (' and ', $where_list);
+  }
+
+  //error_log(json_encode(["$qstr $where_str", $params]));
+  $prep = $sql->prepare("$qstr");
+  $prep->execute($params);
+  return $prep->fetchAll($type);
+}
+
+$_releaseMap = [];
+function get_tracks($label, $release) {
+  global $_releaseMap;
+  if(!isset($_releaseMap["$label:$release"])) {
+    $_releaseMap["$label:$release"] = get("select * from tracks", ['label' => $label, 'release' => $release]);
+  }
+  return $_releaseMap["$label:$release"];
+}
+
+function get_releases($label) {
+  global $_releaseMap;
+  if(!isset($_releaseMap[$label])) {
+    $releaseList = get("select distinct release from tracks", ['label' => $label], PDO::FETCH_COLUMN);
+    mt_srand(floor(time() / (60 * 60 * 24)));
+    shuffle($releaseList);
+    $_releaseMap[$label] = $releaseList;
+  }
+  return $_releaseMap[$label];
+}
+
+$_labelList = false;
+function get_labels() {
+  global $_labelList;
+  if(!$_labelList) {
+    $labelList = get("select distinct label from tracks", [], PDO::FETCH_COLUMN);
+    mt_srand(floor(time() / (60 * 60 * 24)));
+    shuffle($labelList);
+    $_labelList = $labelList;
+  }
+  return $_labelList;
+}
+
+function navigate($label, $release, $direction, $final = false) {
+  $dir = ($direction[0] != '-') * 2 - 1;
+  $what = substr($direction, 1);
+
+  $label_ix = 0;
+  $release_ix = 0;
+
+  $labelList = get_labels();
+
+  if(!$label) {
+    $label = $labelList[0];
+  }
+
+  if($what === 'label') {
+    $ttl = count($labelList);
+    if(!$ttl) {
+      return compact('labelList');
     }
-    $ix = $off;
-    $dir = ($skip[0] != '-') * 2 - 1;
+    if($label) {
+      $label_ix = array_search($label, $labelList);
+    }
+    $label_ix = ($label_ix + $ttl + $dir) % $ttl;
 
-    $what = substr($skip, 1);
-    $base = explode('/',$parts[$off]);
-    $base_len = count($base);
-    $release = $base[$base_len-2];
-    $label = $base[$base_len-3];
-    $start = $off;
+    $label = $labelList[$label_ix];
+    $releaseList = get_releases($label);
+    $release = $releaseList[0];
+    $trackList = get_tracks($label, $release);
 
-    do {
-      $ix = ($ix + $dir) % $ttl;
-      $comp = explode('/',$parts[$ix]);
-      $comp_len = count($comp);
+  } else if ($what === 'release') {
+    $releaseList = get_releases($label);
+    $ttl = count($releaseList);
+    if(!$ttl) {
+      return compact('labelList', 'releaseList');
+    }
+    if($release) {
+      $release_ix = array_search($release, $releaseList);
+    }
+    $release_ix = ($release_ix + $ttl + $dir) % $ttl;
 
-      if(  ($what == 'release' && $comp[$comp_len-2] !== $release)  
-        || ($what == 'label' && $comp[$comp_len-3] !== $label) 
-        ) { 
-        break;
-      }
-    } while ($ix != $off);
-
-    $off = $ix;
+    $release = $releaseList[$release_ix];
+    $trackList = get_tracks($label, $release);
+  } else {
+    if(!$release) {
+      $release = get_releases($label)[0];
+    }
+    $trackList = get_tracks($label, $release);
   }
 
-  $start = 0;
-  $ret = [
-    'ttl' => $ttl,
-    'q' => $search,
-    'res' => [],
-    'off' => $off
+  if($final) {
+    return $trackList;
+  }
+  return [
+    'label' => $label,
+    'release' => $release,
+    'tracks' => $trackList,
+    '+label' => navigate($label, $release, "+label", true)[0],
+    '+release' => navigate($label, $release, "+release", true)[0]
   ];
-  for($ix = $off; $start < 10; $start++) {
-    $ix_off = $mt($ix);
-    $ret['res'][] = $parts[$ix_off];
-    $ix = ($ix + 1) % $ttl;
-  }
 }
 
-
-echo json_encode($ret);
+echo json_encode(navigate(
+  $_GET['label'] ?? false,
+  $_GET['release'] ?? false,
+  $_GET['skip'] ?? "+track"
+));
