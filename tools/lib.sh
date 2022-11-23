f94cb9d9a3cbc7365f2e4f6a899d69e7f313b69f
Chris McKenzie <kristopolous@yahoo.com>
2022-11-23 14:17:05 -0800

    cleanup

--- tools/lib.sh
+++ tools/lib.sh
@@ -382,7 +382,7 @@ tom4a() {
     echo -e "\n\n\n\nEXISTS!!! $out\n\n\n"
   else
     for i in 29 5 2; do
-      ffmpeg -loglevel quiet  -i "$in" -write_xing 0 -id3v2_version 0 -vn -f wav /tmp/out.wav 
+      ffmpeg -nostdin -loglevel quiet  -i "$in" -write_xing 0 -id3v2_version 0 -vn -f wav /tmp/out.wav 
       fdkaac -b 32000 -p $i /tmp/out.wav -o /tmp/out.m4a
       rm /tmp/out.wav
       if [[ -s /tmp/out.m4a ]] ; then
