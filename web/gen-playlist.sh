9ea7d7bb12cc77fdd0f911af491f14b81985786e
Chris McKenzie <kristopolous@yahoo.com>
2024-07-24 01:46:30 -0700

    building conversion in and fixing the sqlite database to be utf8

--- web/gen-playlist.sh
+++ web/gen-playlist.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 music_dir=/raid-real/mp3/label/
+start=$(date +%s)
+dbg() {
+  [[ -n "$INFO" ]] && echo $(( $(date +%s) - start )) "$1"
+}
  
 gen_playlist() {
+  dbg "truncating"
   truncate --size 0 playlist.txt
+  dbg "recreating"
   grep rating_5 "$music_dir"/.listen_done | sort | cut -f 1 -d ' ' | while read i
   do
     ls "$music_dir"/$i/*.mp3 >> playlist.txt
   done
+  dbg "playlist done"
 }
 
 copy_songs() {
   # get just the paths
+  dbg "copying songs"
   grep rating_5 "$music_dir"/.listen_done | sort | cut -f 1 -d ' ' | uniq | awk '{ print "'$music_dir'"$0 }' | while read remotepath; do
     localpath="$(echo "$remotepath" | sed "s/raid-real/raid/")"
     if [[ ! -d "$localpath" ]];then 
@@ -20,21 +28,50 @@ copy_songs() {
     fi
   done
 
+  dbg "copying done"
   sed -i 's/raid-real/raid/g' playlist.txt
 }
 
 convert_songs() {
-  sqlite3 playlist.db "select path from tracks" | while read p; do
-    echo $p
-    mpv-lib toopus $p
-    mpv-lib tom5a $p
+  dbg "converting songs"
+  truncate --size 0 conv_update.sql
+  local n=0
+  sqlite3 playlist.db "select path from tracks where converted = false" > conv_list.txt
+  cat conv_list.txt | while read p; do
+    if [[ ! -s "$p" ]]; then
+      echo "path $p doesn't exist"
+    else 
+      mpv-lib toopus "$p"
+      mpv-lib tom5a "$p"
+      path_m5a="${p/.mp3/.m5a}"
+      path_opus="${p/.mp3/.opus}"
+
+      if [[ -s "$path_m5a" && -s "$path_opus" ]]; then
+        echo -n "."
+        (( n++ ))
+        echo "update tracks set converted=true where path = \"$p\";" >> conv_update.sql
+      else
+        echo "Error for $p:"
+        echo " - ($path_m5a)"
+      fi
+      if (( n > 80 )); then
+        echo ""
+        sqlite3 playlist.db < conv_update.sql
+        truncate --size 0 conv_update.sql
+        n=0
+      fi
+    fi
   done
+  sqlite3 playlist.db < conv_update.sql
+  truncate --size 0 conv_update.sql
+  dbg "converting done"
 }
 
 import_todb() {
   awk -F '/' ' { fuckoff=$0;sub(/-[0-9]*.mp3/, "", $7);print FNR",\""$5"\",\""$6"\",\""$7"\",\""fuckoff"\"" } ' playlist.txt > /tmp/playlist.csv
   {
   sqlite3 playlist.db << ENDL
+PRAGMA encoding=UTF8;
 .mode csv
 .import /tmp/playlist.csv tracks
 update tracks set created_at = current_timestamp where created_at is null;
@@ -57,6 +94,7 @@ as_csv() {
     up integer default 0,
     down integer default 0,
     duration integer default 0,
+    converted boolean default false,
     created_at timestamp default current_timestamp,
     unique(path));
 .mode csv
