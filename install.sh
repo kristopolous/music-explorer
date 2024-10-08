22584f9f65858b19da7f72b63b460a705a7966c2
Chris McKenzie <kristopolous@yahoo.com>
2024-10-08 16:45:37 -0700

    making the yt-dlp dep install easier

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ install_bin() {
 }
 
 install_deps() {
-  list="lua5.4 pulseaudio lua-posix mpv nodejs gawk bash perl bc"
+  list="lua5.4 yt-dlp pulseaudio lua-posix mpv nodejs gawk bash perl bc"
   read -p "I'm a stupid bash script. I'm going to assume you have a system that uses apt. Just press return if I'm correct > " n
 
   if [[ -n "$n" ]]; then
@@ -24,20 +24,6 @@ install_deps() {
   else
     sudo apt -y install $list
   fi
-
-  cat <<ENDL
-  Alright the last thing you need is a fork of youtube-dl. I use yt-dlp, which you need from github (https://github.com/yt-dlp/yt-dlp). You can also use something else:
-
-    * either have a prefs.sh file wher you run mpv-once at with an override of the YTDL variable OR
-    * start things like "$ YTDL=youtube-dl album-get" OR
-    * edit lib.sh yourself and change the YTDL to be whatever you want.
-
-  If things don't work you can invoke things with DEBUG=1 like so:
-
-  $ DEBUG=1 mpv-once 
-
-  or whatever and you'll get more then what you need.
-ENDL
 }
 
 
