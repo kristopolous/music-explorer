a0ef987e55885789c28cad9456815c5a5cdbc980
Chris McKenzie <kristopolous@yahoo.com>
2025-01-15 13:30:09 -0800

    making magic read show stuff better

--- tools/magic-read.py
+++ tools/magic-read.py
@@ -15,7 +15,9 @@ for key, events in selector.select():
     if key.fileobj == sys.stdin:
         print(sys.stdin.readline())
     elif key.fileobj == fifo_fd:
-        print(os.read(fifo_fd, 4096).decode())
+        keystrokes = os.read(fifo_fd, 4096).decode()
+        sys.stderr.write(keystrokes)
+        print(keystrokes)
 
 sys.stdout.flush()
-
+sys.stderr.flush()
